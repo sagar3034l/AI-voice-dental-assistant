85415ae64138cbd2a1a0e5fee24523c6ab5d5401
sagar3034l <sd8778610@gmail.com>
2026-08-13 16:32:51 +0530

    Dashboard-page added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,18 @@
+import ActivityOverview from '@/components/dashboards/ActivityOverview'
+import MainActions from '@/components/dashboards/MainActions'
+import WelcomeSection from '@/components/dashboards/WelcomeSection'
 import Navbar from '@/components/Navbar'
-import React from 'react'
+
 
 function dashboard() {
   return (
     <div>
       <Navbar />
-      DASBOARD
+      <div className='max-w-7xl mx-auto px-6 py-8 pt-24'>
+        <WelcomeSection />  
+        <MainActions />
+        <ActivityOverview />
+      </div>
     </div>
   )
 }
